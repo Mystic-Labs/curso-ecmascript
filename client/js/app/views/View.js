cef9904f916669a7e545e0b8d5167f1802491b7c
arthur mario <artmario@gmail.com>
2020-03-05 15:56:10 -0400

    sobrecarga metodo template

--- client/js/app/views/View.js
+++ client/js/app/views/View.js
@@ -2,7 +2,10 @@ class View {
   constructor(elemento) {
     this._elemento=elemento;
   }
+  template(){
+    throw new Error("metodo deve ser implementado");
+  }
   update(model){
-    this._elemento.innerHTML=this._template(model);
+    this._elemento.innerHTML=this.template(model);
   }
 }
