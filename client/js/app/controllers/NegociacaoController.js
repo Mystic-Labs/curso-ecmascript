fc087333b2119d9ecc2843af7ed98f65734d0a1c
arthur mario <artmario@gmail.com>
2020-03-05 11:01:51 -0400

    date helper statico com template string

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -10,18 +10,14 @@ class NegociacaoController {
   adiciona(event) {
     event.preventDefault();
     //console.log(typeof (this._inputData.value));
-
-
-    let data = new Date(
-      ...this._inputData.value
-        .split('-')
-        .map((item, indice) => item - indice % 2)
-    );
+    let data = DateHelper.textoParaData(this._inputData.value);
+    console.log(data);
     let negociacao = new Negociacao(
       data,
       this._inputQuantidade.value,
       this._inputValor.value
     );
-    console.log(negociacao);
+
+    console.log(DateHelper.dataParaTexto(negociacao.data));
   }
 }
